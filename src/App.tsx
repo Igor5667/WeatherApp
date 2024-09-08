d356f99352d6f688141574a141c1a1283d56941f
Igor5567 <igidod@gmail.com>
2024-09-08 15:54:52 +0200

    init, fetching data from openWeather

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+function App() {
+  const [weatherData, setWeatherData] = useState<any>({});
+
+  const fetchApi = async () => {
+    await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=756edc072ecb1a813e71e3ffd09e683c" //don't panic I'll delete this key
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setWeatherData(data);
+        console.log(data);
+      })
+      .catch((err) => console.log("HUSTON MAMY PROBLEM", err));
+  };
+
+  return (
+    <div className="h-screen w-screen flex justify-center flex-col items-center">
+      <button
+        className="bg-red-600 text-white text-2xl font-semibold p-5 rounded-lg hover:bg-red-700 shadow-sm 
+        active:translate-y-[5px] active:shadow-none transition"
+        onClick={fetchApi}
+      >
+        CLICK ME AND CHECK OUT DOWN HERE
+      </button>
+      <p className="text-wrap">{weatherData["name"] ?? "NO DATA YET"}</p>
+    </div>
+  );
+}
+
+export default App;
