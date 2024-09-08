606e2c11290ba6d9491c7fa131ecddf52561425b
Igor5567 <igidod@gmail.com>
2024-09-08 19:17:40 +0200

    reafactor fetch to SearchBar, add handleSearch

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,9 @@ import Main from "./components/Main";
 function App() {
   const [weatherData, setWeatherData] = useState<any>({});
 
-  const fetchApi = async () => {
-    await fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=Dubai&units=metric&appid=756edc072ecb1a813e71e3ffd09e683c" //don't panic I'll delete this key
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        setWeatherData(data);
-        console.log(data);
-      })
-      .catch((err) => console.log("HUSTON MAMY PROBLEM", err));
-  };
-
   return (
     <div className="h-screen w-screen flex justify-center flex-col items-center bg-main-background">
-      <SearchBar />
+      <SearchBar setWeatherData={setWeatherData} />
       <Main />
       <li className="text-wrap list-none">
         {weatherData["name"] ?? "NO DATA YET"}
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,14 +1,44 @@
+import { useState } from "react";
 import { IoSearch } from "react-icons/io5";
 
-function SearchBar() {
+interface SearchBarProps {
+  setWeatherData: (data: any) => void;
+}
+
+function SearchBar({ setWeatherData }: SearchBarProps) {
+  const [searchInputValue, setSearchInputValue] = useState("");
+
+  const fetchApi = async () => {
+    await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=Dubai&units=metric&appid=756edc072ecb1a813e71e3ffd09e683c" //don't panic I'll delete this key
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setWeatherData(data);
+        console.log(data);
+      })
+      .catch((err) => console.log("ERROR WHILE FETCHING", err));
+  };
+
+  const handleSearch = () => {
+    let searchPhrase = searchInputValue.trim();
+    if (searchPhrase === "") return;
+    fetchApi();
+  };
+
   return (
     <div className="w-full flex justify-center gap-5 p-5 text-lg">
       <input
+        value={searchInputValue}
+        onChange={(e) => setSearchInputValue(e.target.value)}
         type="text"
         className="w-[70%] p-2 px-5 focus:outline-none rounded-full"
         placeholder="Search for a city"
       />
-      <button className="rounded-full bg-white w-12 aspect-square p-3">
+      <button
+        className="rounded-full bg-white w-12 aspect-square p-3"
+        onClick={handleSearch}
+      >
         <IoSearch className="w-full h-full" />
       </button>
     </div>
